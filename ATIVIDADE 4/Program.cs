03669f7ee0eada2cf38c05951996864f94a06f62
ANALICE-CTDS20 <silvaa2465@gmail.com>
2025-10-02 17:13:21 -0300

    Add files via upload

---
+++ ATIVIDADE 4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite sua idade em anos: ");
+int anos = int.Parse(Console.ReadLine());
+
+Console.Write("Digite meses: ");
+int meses = int.Parse(Console.ReadLine());
+
+Console.Write("Digite dias: ");
+int dias = int.Parse(Console.ReadLine());
+
+int totalDias = anos * 365 + meses * 30 + dias;
+
+Console.WriteLine($"A idadde em dias é: {totalDias}");
