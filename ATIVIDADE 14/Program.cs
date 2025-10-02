ea3fe320197b793cc3e276d958f8fd578501d4e3
ANALICE-CTDS20 <silvaa2465@gmail.com>
2025-10-02 17:19:07 -0300

    Add files via upload

---
+++ ATIVIDADE 14/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite um valor N (maior que 0): ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"\nNumeros de 1 ate {N}:");
+for (int i = 1; i <= N; i++)
+
+    Console.WriteLine(i);
+
+Console.WriteLine("\nPressione Enter para sair...");
+Console.ReadLine();
