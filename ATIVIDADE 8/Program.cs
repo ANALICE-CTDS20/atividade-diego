2a1d241f197fe435b6980bf7a7794191439a2c32
ANALICE-CTDS20 <silvaa2465@gmail.com>
2025-10-02 17:14:40 -0300

    Add files via upload

---
+++ ATIVIDADE 8/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Write("Digite um valor diferente de zero: ");
+double valor = double.Parse(Console.ReadLine());
+
+if (valor > 0)
+    Console.WriteLine("POSITIVO")
+        else
+    Console.WriteLine("NEGATIVO");
